23436fbc2563dfafb286db7ef17c7c1c60f7752c
Nikolay <vnovus46@gmail.com>
2023-03-12 22:06:24 +0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int amount = 0;
+        int refill = 700;
+        int bonus = 0;
+        if (refill > 500) {
+            bonus = (refill) / 100;
+        }
+        int total = amount + refill + bonus;
+
+        System.out.println("Бонусы- " + bonus);
+        System.out.println("Итоговая сумма- " +total);
+
+
+
     }
 }
